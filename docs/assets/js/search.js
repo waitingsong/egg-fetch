92bf2dd72d25c6d3f5175be71536f7b47d87fd30
waiting <1661926154@qq.com>
2019-09-05 16:00:56 +0800

    docs: add documentation

---
+++ docs/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"256":"Interface","1024":"Property"},"rows":[{"id":0,"kind":256,"name":"FetchConfig","url":"interfaces/fetchconfig.html","classes":"tsd-kind-interface"},{"id":1,"kind":1024,"name":"agent","url":"interfaces/fetchconfig.html#agent","classes":"tsd-kind-property tsd-parent-kind-interface","parent":"FetchConfig"},{"id":2,"kind":1024,"name":"app","url":"interfaces/fetchconfig.html#app","classes":"tsd-kind-property tsd-parent-kind-interface","parent":"FetchConfig"},{"id":3,"kind":1024,"name":"client","url":"interfaces/fetchconfig.html#client","classes":"tsd-kind-property tsd-parent-kind-interface","parent":"FetchConfig"}]};
